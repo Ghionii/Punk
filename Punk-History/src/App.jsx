1ad31bd4fd1fc1b7540f777108b5122b9340c471
Ghionii <148452291+Ghionii@users.noreply.github.com>
2024-03-13 14:44:09 +0100

    Added Puanks bands

--- Punk-History/src/App.jsx
+++ Punk-History/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Nav from './Components/Nav';
+import Bands from './Components/Bands';
 
 function App() {
-  return <></>;
   return (
     <>
       <Header />
       <Nav />
-
+      <Bands />
     </>
   );
 }
